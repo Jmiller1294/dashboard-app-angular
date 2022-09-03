aa0f807044920a32576154bbfa538f685b9adeba
Justin Miller <jayman1294@gmail.com>
2022-09-03 02:45:42 -0400

    add form controls to tasks form

--- src/app/widget/widgets/tasks-widget.component.ts
+++ src/app/widget/widgets/tasks-widget.component.ts
@@ -1,43 +1,51 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'tasks-widget',
   template: `
     <div class="tasks-container">
         <h2>Tasks</h2>
-        <form id="tasks-form" 
+        <form class="tasks-input-form" 
           [formGroup]="tasksForm" 
-          (keyup.enter)="onEnterKeyPress($event)">
+          (keyup.enter)="onEnterKeyPress()">
           <textarea class="tasks-input-box"
-           formControlName="tasks">
+            formControlName="tasks-input">
           </textarea>
+          <div formArrayName="tasks" 
+            class="tasks-list-form" 
+            *ngFor="let taskControl of tasksArrayControl; let i = index"
+            >
+              <input type="checkbox" id="task" [formControlName]="i">
+              <label for="vehicle1"> {{taskControl.getRawValue()}}</label><br>
+          </div>
         </form>
-        <ul class="tasks-list" *ngFor="let task of tasksArray">
-          <li>
-            <input type="checkbox" id="task">
-            <label for="vehicle1">{{task}}</label><br>  
-          </li>
-        </ul>
     </div>
   `,
   styleUrls: ['../widget.component.css'],
 })
 export class TasksWidgetComponent implements OnInit {
   tasksForm: FormGroup;
-  tasksArray: Array<string>;
-
+  console = console;
   constructor() { }
 
   ngOnInit(): void {
     this.tasksForm = new FormGroup({
-      'tasks': new FormControl(null)
+      'tasks-input': new FormControl(null),
+      'tasks': new FormArray([])
     });
   }
 
-  onEnterKeyPress(event: any) {
-    this.tasksArray = event.target.value.split(",");
-    console.log(this.tasksArray);
+  get tasksArrayControl() {
+    return (this.tasksForm.get('tasks') as FormArray).controls;
   }
 
+  onEnterKeyPress() {
+    const tasks = this.tasksForm.get('tasks-input')?.value.split(",");
+    tasks.map((task: any) => {
+      const control = new FormControl(task);
+      (<FormArray>this.tasksForm.get('tasks')).push(control);
+    })
+    this.tasksForm.get('tasks-input')?.setValue(null);
+  }
 }
