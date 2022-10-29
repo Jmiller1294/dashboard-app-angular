0f2eaf9ca8b61b4d6f78f6701a23bdcf8085f85c
Justin Miller <jayman1294@gmail.com>
2022-10-29 15:56:11 -0400

    add paths for image icons

--- src/app/features/sidebar/sidebar.component.ts
+++ src/app/features/sidebar/sidebar.component.ts
@@ -3,27 +3,33 @@ import { Component, OnInit } from '@angular/core';
 export const images = [
   {
     class: "home-icon",
-    url: '../../../assets/icons8-home-grey.png'
+    url: '../../../assets/icons8-home-grey.png',
+    path: '/'
   },
   {
     class: "calender-icon",
-    url: '../../../assets/icons8-calendar-grey.png'
+    url: '../../../assets/icons8-calendar-grey.png',
+    path: '/calender'
   },
   {
     class: "library-icon",
-    url: '../../../assets/icons8-book-shelf-grey.png'
+    url: '../../../assets/icons8-book-shelf-grey.png',
+    path: '/library'
   },
   {
     class: "stocks-icon",
-    url: '../../../assets/icons8-stocks-grey.png'
+    url: '../../../assets/icons8-stocks-grey.png',
+    path: 'stocks'
   },
   {
     class: "list-icon",
-    url: '../../../assets/icons8-list-grey.png'
+    url: '../../../assets/icons8-list-grey.png',
+    path: '/list'
   },
   {
     class: "settings-icon",
-    url: '../../../assets/icons8-engineering-grey.png'
+    url: '../../../assets/icons8-engineering-grey.png',
+    path: '/settings'
   },
 ]
 
